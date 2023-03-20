108da47f1f43f6d9c3cb4b31cbaec1602184df8f
Carter Dunn <111713600+CarCar3400@users.noreply.github.com>
2023-03-20 21:59:21 +0000

    Mon, Mar 20, 2023, 2:59 PM -07:00

--- js/jokes.js
+++ js/jokes.js
@@ -1,14 +1,18 @@
-const jokeE1 = document.getElementById('joke')
+const jokeEl = document.getElementById('joke')
 const jokeBtn = document.getElementById('jokeBtn')
+
 
 generateJoke()
 
+
 jokeBtn.addEventListener('click', generateJoke)
 
-async function generateJoke(){
-    const response = await fetch(https://icanhazdadjoke.com', {
-        headers: {Accept: 'application'/json}
+
+async function generateJoke() {
+    const response = await fetch('https://icanhazdadjoke.com', {
+        headers: {Accept: 'application/json'}
     })
+
 
     const data = await response.json()
     jokeEl.innerHTML = data.joke
