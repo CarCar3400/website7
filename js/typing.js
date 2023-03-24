7afce1913da598286b8eddd84f77714fa4d67489
Carter Dunn <111713600+CarCar3400@users.noreply.github.com>
2023-03-24 21:57:30 +0000

    Fri, Mar 24, 2023, 2:57 PM -07:00

---
+++ js/typing.js
@@ -0,0 +1,12 @@
+const textEl = document.getElementById('text')
+const speedEl = document.getElementById('speed')
+const text = "I wuv petite wittle Cody Kens :3"
+let idx = 1
+let speed = 300
+
+writeText()
+
+function writeText(){
+    textEl.innerText = text.slice(0,1)
+
+}
